d61c5930983140b82f6b81586cc41561aa6dbe4b
Kenan <ktasdemiriletisim@gmail.com>
2025-06-29 12:07:20 +0300

    Initial Commit

---
+++ eticaretappui/eticaretappuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  eticaretappuiApp.swift
+//  eticaretappui
+//
+//  Created by kenan on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct eticaretappuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ eticaretappuiTests/eticaretappuiTests.swift
@@ -0,0 +1,17 @@
+//
+//  eticaretappuiTests.swift
+//  eticaretappuiTests
+//
+//  Created by kenan on 6/29/25.
+//
+
+import Testing
+@testable import eticaretappui
+
+struct eticaretappuiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
